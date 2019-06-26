446d650d999ecad01be529613b1530481293e604
Brian Chiang <brianchiang1988@icloud.com>
2019-06-26 14:50:13 +0800

    complete dot product

--- Assignment_bootstrap/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.py
+++ Assignment_bootstrap/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.py
@@ -4,10 +4,25 @@ import sys
 
 def max_dot_product(a, b):
     #write your code here
-    res = 0
-    for i in range(len(a)):
-        res += a[i] * b[i]
-    return res
+    
+
+    # print("a", a)
+    # print("b", b)
+
+    # Sort vector a and vector b on descending order
+    a.sort( reverse=True )
+    b.sort( reverse=True )
+
+    # print("a", a)
+    # print("b", b)
+
+    # Concept: maximum sum of product comes from each maximum product on every index
+    sum_of_product = 0
+    for i in range( len(a) ):
+        sum_of_product += a[i] * b[i]
+    return sum_of_product
+
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -15,5 +30,5 @@ if __name__ == '__main__':
     n = data[0]
     a = data[1:(n + 1)]
     b = data[(n + 1):]
-    print(max_dot_product(a, b))
+    print( max_dot_product(a, b) )
     
