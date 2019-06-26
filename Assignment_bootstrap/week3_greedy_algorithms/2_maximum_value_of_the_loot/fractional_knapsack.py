2bfabdef059358d65efd436c82b7cafb4ed89675
Brian Chiang <brianchiang1988@icloud.com>
2019-06-26 12:40:47 +0800

    complete assignment of fractional knapsack complete assignment of car fueling

--- Assignment_bootstrap/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ Assignment_bootstrap/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -2,16 +2,64 @@
 import sys
 
 def get_optimal_value(capacity, weights, values):
+
+    # initialize value of pick
     value = 0.
-    # write your code here
+
+    # get total number of item
+    number_of_items = len(weights)
+
+    # make a item pair by corresponding values and weights
+    item_pair = list( zip(values, weights) )
+
+
+    # Sort by value per unit weight, on descending order
+    # item[0]: total value
+    # item[0]: total weight
+    item_pair = sorted( item_pair, key = lambda item: (item[0]/item[1]), reverse = True )
+    
+
+
+    # initialize the weight of pick
+    weight_of_pick = [0] * number_of_items
+
+    # initialize the remaining capacity
+    remaining_capacity = capacity
+
+    # initialize the index 
+    index = 0
+
+
+
+    while sum(weight_of_pick) != capacity:
+
+        # max load = min of { the weight of most worthy item, remaining capacity }
+        maximam_load_of_most_worthy_item = min( item_pair[index][1], remaining_capacity )
+
+        # update weight of pick on most worthy item
+        weight_of_pick[index] = maximam_load_of_most_worthy_item
+
+        # update remaining capacity
+        remaining_capacity = remaining_capacity - weight_of_pick[index]
+
+        # value per unit weight of item
+        value_per_unit_weight = item_pair[index][0]/item_pair[index][1]
+
+        # update value of pick
+        value = value + weight_of_pick[index] * value_per_unit_weight
+
+        # pick the second worthy item on next iteration
+        index += 1
 
     return value
 
 
+
+# Entry point of program
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
+    data = list( map(int, sys.stdin.read().split() ) )
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+    print("{:.4f}".format(opt_value))
--- Assignment_bootstrap/week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ Assignment_bootstrap/week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -1,11 +1,76 @@
 # python3
 import sys
 
+def get_reachable_gas_station( current_position, miles_to_empty, stops ):
 
+    reachable_gas_station = []
+
+    for gas_station_position in stops:
+
+        # keep going further, instead of being on reverse direction
+        if (gas_station_position > current_position) and (gas_station_position - current_position) < miles_to_empty:
+            reachable_gas_station.append( gas_station_position )
+            # stops.remove( gas_station_position )
+
+
+    # print("\n\n")
+    # print("currnet_position", current_position)
+    # print("miles_to_empty", miles_to_empty)
+    # print("stops", stops )
+    # print("reachable_gas_station", reachable_gas_station)
+
+
+    if not reachable_gas_station:
+        # cannot reach next gas station from current positiotn
+        return [-1]
+    else:
+        return reachable_gas_station
+
+
+
+# core function to compute min refuel times
 def compute_min_refills(distance, tank, stops):
-    # write your code here
-    return -1
+    
+    # number_of_gas_stations = len( stops )
 
+    # initialize current_position as start point 0 miles
+    last_refuel_position = 0
+
+    # initialize furthest_travel_range to (0 + tank) miles
+    furthest_travel_range = 0 + tank
+
+    # initialize times of refuel to 0
+    times_of_refuel = 0
+
+    # keeps going until meets the end
+    while furthest_travel_range < distance:
+
+        gas_station_in_range = get_reachable_gas_station(last_refuel_position, tank, stops)
+
+        # minimize the times of refuel by going as futher as possible
+        furthest_refuel_stop = max( gas_station_in_range )
+
+        if -1 == furthest_refuel_stop:
+            # this car cannot reach the end
+            return -1
+        else:
+            
+            # refules in gas station with furthest_refuel_stop (on miles)
+            last_refuel_position = furthest_refuel_stop
+
+            # update furtjest travel range
+            furthest_travel_range = last_refuel_position + tank
+
+            # update times of refuel
+            times_of_refuel += 1
+    
+
+
+    return times_of_refuel
+
+
+
+# Entry point of program
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
     print(compute_min_refills(d, m, stops))
