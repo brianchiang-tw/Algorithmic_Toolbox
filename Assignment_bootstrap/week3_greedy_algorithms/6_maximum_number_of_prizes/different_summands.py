940d16bbed78cc447d4901bab18e5fceea13e903
Brian Chiang <brianchiang1988@icloud.com>
2019-06-26 16:43:36 +0800

    complete different summands

--- Assignment_bootstrap/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ Assignment_bootstrap/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,9 +1,37 @@
 # Uses python3
 import sys
 
+def total_sum_of_ascending_series(k):
+
+    # series_sum = 1 + 2 + ... + k = k * ( k + 1) /2
+    # this must be a integer, so we adopt integer division here.
+    series_sum = k*(k+1)//2
+
+    return series_sum
+
+
+
 def optimal_summands(n):
     summands = []
     #write your code here
+
+    upper_bound = int( (2*n)**(1/2)+1 )
+
+    for k in range(1, upper_bound):
+        current_sum = total_sum_of_ascending_series( k )
+        next_sum = total_sum_of_ascending_series( k+1 )
+
+        if current_sum <= n < next_sum:
+            delta =  n - current_sum 
+
+            # make a ascending series from 1 to k (including k)
+            summands = list( range(1, k+1) )
+
+            # compensate final term for delta to meets the value of n
+            summands[-1] +=  delta
+            break
+
+
     return summands
 
 if __name__ == '__main__':
