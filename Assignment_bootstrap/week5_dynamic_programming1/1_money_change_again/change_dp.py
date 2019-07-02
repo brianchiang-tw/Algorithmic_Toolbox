c43968814836514f05acfcf204a58b27d060b4c5
Brian Chiang <brianchiang1988@icloud.com>
2019-07-03 00:21:15 +0800

    refine the comment of change_dp.py

--- Assignment_bootstrap/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ Assignment_bootstrap/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -10,67 +10,46 @@ def get_min_change( price, solution_table ):
 
         if 0 == p:
             solution_table[p] = ( 0, [] )
-            # print("sol at p",  solution_table[p] )
 
         elif 1 == p:
             solution_table[p] = ( 1, [1] )    
-            # print("sol at p",  solution_table[p] )
 
         elif 3 == p:
             solution_table[p] = ( 1, [3] )
-            # print("sol at p",  solution_table[p] )
 
         elif 4 == p:
             solution_table[p] = ( 1, [4] )
-            # print("sol at p",  solution_table[p] )
 
         elif 2 == p:
             solution_table[p] = ( 2, [1, 1])
-            # print("sol at p",  solution_table[p] )
         
         else:
             
             min_coin_of_price_p_minus_k = noNones(min)(  solution_table[p-1][0],  solution_table[p-3][0], solution_table[p-4][0] )
-            # print("min_coin_of_price_p_minus_k:", min_coin_of_price_p_minus_k)
 
 
             if min_coin_of_price_p_minus_k == solution_table[p-1][0]:
-                # print("add 1")
                 # add a coin with 1 dollar
 
-                # print("container of p-1", solution_table[p-1][1] )
                 container = list( solution_table[p-1][1] )
                 container.append(1)
-                # print("container of p", container)
                 solution_table[p] = ( min_coin_of_price_p_minus_k + 1, container )
-                # print("sol at p",  solution_table[p] )
 
             elif min_coin_of_price_p_minus_k == solution_table[p-3][0]:
-                # print("add 3")
                 # add a coin of 3 dollar
 
-                # print("p", p)
-                # print( solution_table[p-3] )
                 container = list( solution_table[p-3][1] )
-                # print("before adding 3")
-                # print("container", container)
                 container.append(3)
                 solution_table[p] = ( min_coin_of_price_p_minus_k + 1, container )
-                # print("sol at p",  solution_table[p] )
 
             else:
-
+                # add a coin of 4 dollar
 
                 container = list( solution_table[p-4][1] )
-                # print("before adding 4")
                 container.append(4)
-                # print("add 4")
-                # add a coin of 4 dollar
                 solution_table[p] = ( min_coin_of_price_p_minus_k + 1, container )
-                # print("sol at p",  solution_table[p] )
 
 
-        # print("sol table", solution_table )
 
     return solution_table[price][0], solution_table[price][1]
 
