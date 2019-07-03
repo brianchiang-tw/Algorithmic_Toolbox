c50856e0c275e62028459b912c60076b22435b3b
Brian Chiang <brianchiang1988@icloud.com>
2019-07-03 21:00:04 +0800

    refine the comment of lcs3.py ( longest common sequence among three strings )

--- Assignment_bootstrap/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ Assignment_bootstrap/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -13,14 +13,13 @@ def longest_common_sequence(s, t, u):
     t = [0] + t
     u = [0] + u
 
+    # get length from input s, t, and u
     depth = len(u)
     height = len(t)
     width = len(s)
 
 
-    # The max length of common sequence between string s to empty string is 0
-    # The max length of common sequence between string t to empty string is 0
-    # The max length of common sequence between string u to empty string is 0
+
     # create a 3d array of depth * height * width
     length_of_common_sequence = [ [ [ 0 for x in range(width) ] for y in range(height) ] for z in range(depth) ]
 
@@ -28,19 +27,19 @@ def longest_common_sequence(s, t, u):
     
     # Main idea of minimal distance with dynamic programming
 
-    # s: the input string (or sequence)
-    # t: the input string (or sequence)
-    # u: the input string (or sequence)
+    # s: the input sequence
+    # t: the input sequence
+    # u: the input sequence
 
-    # s-1: substring of s without tail character, where s-1 = s[0:-1]
-    # t-1: substring of t without tail character, where t-1 = t[0:-1]
-    # u-1: substring of t without tail character, where u-1 = u[0:-1]
+    # s-1: sub-sequence of s without tail character, where s-1 = s[0:-1]
+    # t-1: sub-sequence of t without tail character, where t-1 = t[0:-1]
+    # u-1: sub-sequence of t without tail character, where u-1 = u[0:-1]
 
-    # If tail character among s, t and u is equal, 
+    # If tail element among s, t and u is equal, 
     #
     # the maximal length_of_common_sequence(s,t) = length_of_common_sequence(s-1, j-1, u-1) + 1 [ Note: +1 is due to the equality tail of s = tail of t = tail of u]
     
-    # If tail characters of s, t, and u, are different, 
+    # If tail elements of s, t, and u, are different, 
     #
     # the maximal length_of_common_sequence(s,t) = max  {   
     #                                                       length_of_common_sequence(s-1, t  , u   ) 
@@ -58,17 +57,24 @@ def longest_common_sequence(s, t, u):
                 max_length_of_common_seq_by_deleting_s_tail = length_of_common_sequence[ i   ][ j   ][ k-1 ]
                 max_length_of_common_seq_by_match           = length_of_common_sequence[ i-1 ][ j-1 ][ k-1 ] + 1
 
+
+
                 if i == 0 or j == 0 or k == 0:
+                    # The max length of common sequence between string s to empty string is 0
+                    # The max length of common sequence between string t to empty string is 0
+                    # The max length of common sequence between string u to empty string is 0
                     length_of_common_sequence[i][j][k] = 0
 
                 elif u[i] == t[j] and t[j] == s[k]:
+                    # tail element among s, t and u is equal
                     length_of_common_sequence[i][j][k] = max_length_of_common_seq_by_match
 
                 else:
+                    # tail elements of s, t, and u, are different 
                     length_of_common_sequence[i][j][k] = max( max_length_of_common_seq_by_deleting_s_tail, max_length_of_common_seq_by_deleting_t_tail, max_length_of_common_seq_by_deleting_u_tail )
 
 
-
+    # output length of longest common sequence of three series
     return length_of_common_sequence[ depth-1 ][ height-1 ][ width-1 ]
 
 
