55176ae7e00225ffc3f71589e0cbd09c1da90ab0
Brian Chiang <brianchiang1988@icloud.com>
2019-07-04 18:14:21 +0800

    complete knapsack.py (  discrete_0_1_knapsack problem )

--- Assignment_bootstrap/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ Assignment_bootstrap/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,13 +1,92 @@
 # Uses python3
 import sys
+import os
+
+
+
+def print_2d_array( image_matrix, mode = "Hex" ):
+
+    image_height = len( image_matrix )
+
+
+    image_width = len( image_matrix[0] )
+
+
+    for y in range( image_height ):
+        for x in range( image_width ):
+
+            if "Hex" == mode:
+                print( "{:02X}".format( image_matrix[y][x] ), end = ' ' )
+            elif "Decimal" == mode:
+                print( "{: 10d}".format( image_matrix[y][x] ), end = ' ' )
+            else:
+                print("Invliad print mode  {:<10} is not supported.".format(mode) )
+
+        print()    
+
+    # One more extra new line in order to keep output neat and tidy
+    print("\n")
+
+
+
+def discrete_0_or_1_knapsack( capacity, item_list, lookup_table):
+
+    if not item_list or capacity < 0:
+        # item_list is empty, max weight = 0
+        # capacity is negative(cannot take item anymore), max weight = 0
+        return 0
+
+    else:
+
+        if 0 != lookup_table[len(item_list)][capacity]:
+            # early return if lookup table has result of maximal weight on sub-problem
+            return lookup_table[len(item_list)][capacity]
+
+        else:
+
+
+
+            # remove and return last item (i.e., item_i)
+            item_i = item_list[-1]
+
+            # weight_of_picking_item_i = 0
+            if(  capacity-item_i >= 0):
+                # get the weight of picking item_i
+                weight_of_picking_item_i = discrete_0_or_1_knapsack( capacity-item_i, item_list[0:-1], lookup_table) + item_i
+
+            else:
+                # can not picking item_i because remaining space: (capacity-item_i) is negative (can not take anymore)
+                weight_of_picking_item_i = 0
+
+
+            # get the weight of not picking item_i
+            weight_of_not_picking_item_i = discrete_0_or_1_knapsack( capacity, item_list[0:-1], lookup_table) + 0
+
+            # pick maximal weight
+            max_weight = max(weight_of_picking_item_i, weight_of_not_picking_item_i)
+
+            # update lookup table
+            lookup_table[len(item_list)][capacity] = max_weight
+
+            return max_weight
+
+
 
 def optimal_weight(W, w):
     # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+
+    # W: Max capacity of bag
+    # w: list of gold with weight_#i
+
+
+    lookup_table = [ [ 0 for x in range(W+1) ] for u in range( len(w)+1) ]
+
+    max_weight = discrete_0_or_1_knapsack( W, w, lookup_table )
+
+    # debug message
+    # print_2d_array( lookup_table )
+
+    return max_weight
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- Assignment_bootstrap/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ Assignment_bootstrap/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -2,17 +2,84 @@
 import sys
 import itertools
 
+def discrete_0_or_1_knapsack( capacity, item_list, lookup_table):
+
+
+    match_of_capacity = 0
+
+    for y in range( 1, len(lookup_table) ):
+        for current_capacity in range( 1, len(lookup_table[0]) ):
+
+            item_i = y - 1
+
+            
+            if(  current_capacity - item_list[item_i] >= 0):
+                # get the weight of picking item_i
+                weight_of_picking_item_i = lookup_table[ y-1 ][ current_capacity - item_list[item_i] ] + item_list[item_i]
+
+            else:
+                # can not picking item_i because remaining space: (capacity-item_i) is negative (can not take anymore)
+                weight_of_picking_item_i = 0            
+
+            # get the weight of not picking item_i
+            weight_of_not_picking_item_i = lookup_table[y-1][ current_capacity ] + 0
+
+            max_weight = max(weight_of_picking_item_i, weight_of_not_picking_item_i)
+
+            lookup_table[y][current_capacity] = max_weight
+
+            if capacity == max_weight:
+                # if capacity is matched, then there is a subset of summation = capacity
+                match_of_capacity += 1
+
+
+    if match_of_capacity >= 3:
+        # There are 3 subsets with equal sum
+        return 1
+
+    else:
+        # Cannoy make 3 subsets with equal sum
+        return 0
+
+
+
+# preprocessing 
+def find_3_partition_of_equal_sum( target_sum, series):
+    # write your code here
+
+    # target_sum: sum value of subset of 3-partition
+    # series: input sequence of numbers
+
+
+    lookup_table = [ [ 0 for x in range(target_sum+1) ] for u in range( len(series)+1) ]
+
+    existence_of_3_partition_of_equal_sum = discrete_0_or_1_knapsack( target_sum, series, lookup_table )
+
+    # debug message
+    # print_2d_array( lookup_table )
+
+    return existence_of_3_partition_of_equal_sum
+
+
+
+# function trigger
 def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
-        sums = [None] * 3
-        for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
 
-        if sums[0] == sums[1] and sums[1] == sums[2]:
-            return 1
 
-    return 0
+    sum_value = sum(A)
 
+    if 0 != sum_value % 3 or len(A) < 3:
+        # There is no perfect 3 subset of equal sum if one of those following conditions is True
+        # 1. sum_value of A is not dividable by 3
+        # 2. total elements number of A is smaller than 3
+        return 0
+
+    else:
+        return find_3_partition_of_equal_sum( target_sum = sum_value // 3, series = A)
+
+    
+
+# Entry point of program
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *A = list(map(int, input.split()))
